29fe5b963006d3270dc76c25b3be13cf74789407
nagizero <nagizero@foxmail.com>
2018-09-14 10:09:48 +0800

    1

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-python exp_vqa/eval_vqa2.py --gpu_id 2 --exp_name or_butd_rl --snapshot_name 00160000 --test_split test2015 --butd 1
+python exp_vqa/eval_vqa2.py --gpu_id 2 --exp_name butd_wo_expert --snapshot_name 00160000 --test_split test2015 --butd 1
